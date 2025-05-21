0e6916472ce1b8424e03da5e344a6c54c63f63be
hxrixi <harinimohan385@gmail.com>
2025-05-21 23:49:00 +0530

    Update userinputDemo.java

--- userinputDemo.java
+++ userinputDemo.java
@@ -7,20 +7,20 @@ public class userinputDemo {
         String name=scan.nextLine();
         System.out.println("Enter your age:");
         int age=scan.nextInt();
-        System.err.println("Enter your mark of Tamil:");
+        System.err.println("Enter your mark of subject1:");
         int sub1=scan.nextInt();
-        System.out.println("Enter your mark of English:");
+        System.out.println("Enter your mark of subject2:");
         int sub2=scan.nextInt();
-        System.out.println("Enter your mark of maths:");
+        System.out.println("Enter your mark of subject3:");
         int sub3=scan.nextInt();
-        System.out.println("Enter your mark of java:");
+        System.out.println("Enter your mark of subject4:");
         int sub4=scan.nextInt();
-        System.out.println("Enter your mark of DS:");
+        System.out.println("Enter your mark of subject5:");
         int sub5=scan.nextInt();
         int Total=(sub1+sub2+sub3+sub4+sub5);
         int avg=(Total/5);
         if(avg==100){
-        System.out.println("you are a rockstar");
+        System.out.println("GRADE O");
         }else if(avg>=90 && avg<=99){
             System.out.println("GRADE A");
         }else if(avg>=75 && avg<=89){
